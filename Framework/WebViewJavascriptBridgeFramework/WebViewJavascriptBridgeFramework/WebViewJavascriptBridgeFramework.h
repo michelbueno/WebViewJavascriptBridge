80da8c7917414658833aaa6f57b8607528c91834
Michel Ferreira <mbueno@thoughtworks.com>
2016-10-27 10:54:58 -0300

    Creates a framework project

---
+++ Framework/WebViewJavascriptBridgeFramework/WebViewJavascriptBridgeFramework/WebViewJavascriptBridgeFramework.h
@@ -0,0 +1,20 @@
+//
+//  WebViewJavascriptBridgeFramework.h
+//  WebViewJavascriptBridgeFramework
+//
+//  Created by Michel Bueno on 10/27/16.
+//  Copyright © 2016 Michel Bueno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebViewJavascriptBridgeFramework.
+FOUNDATION_EXPORT double WebViewJavascriptBridgeFrameworkVersionNumber;
+
+//! Project version string for WebViewJavascriptBridgeFramework.
+FOUNDATION_EXPORT const unsigned char WebViewJavascriptBridgeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebViewJavascriptBridgeFramework/PublicHeader.h>
+
+#import <WebViewJavascriptBridgeFramework/WebViewJavascriptBridge.h>
+#import <WebViewJavascriptBridgeFramework/WKWebViewJavascriptBridge.h>
